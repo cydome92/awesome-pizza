30a626bad0833b7ce13876b8bf5b1e79a1f878f4
Domenico Zagaria <domenicozagaria1@gmail.com>
2025-03-28 16:35:21 +0100

    fix(AwesomePizzaApplicationTests)
    corretto nome classe main e aggiunta annotation suggerita da spring boot per la serializzazione di Page<?>

--- src/test/java/dev/cydome/awesomepizza/AwesomepizzaApplicationTests.java
+++ src/test/java/dev/cydome/awesomepizza/AwesomepizzaApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
-class AwsomepizzaApplicationTests {
+class AwesomepizzaApplicationTests {
 
 	@Test
 	void contextLoads() {
