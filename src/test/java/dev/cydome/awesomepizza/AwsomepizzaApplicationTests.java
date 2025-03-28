183b193995b7acdc0f4836e3d59feb2a27335263
Domenico Zagaria <domenicozagaria1@gmail.com>
2025-03-28 16:29:13 +0100

    build(AwesomePizzaApplicationTests)
    forzato l'utilizzo della main class in fase di test

--- src/test/java/dev/cydome/awesomepizza/AwsomepizzaApplicationTests.java
+++ src/test/java/dev/cydome/awesomepizza/AwsomepizzaApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 class AwsomepizzaApplicationTests {
 
 	@Test
